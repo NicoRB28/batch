cdc0e283873163a710738fc3fbf523aca196c589
NicoRB28 <nicolasrodriguezboschi@gmail.com>
2022-09-28 00:34:01 -0300

    nueva implementacion con JdbcPagingItemReader

--- src/main/java/com/examplebatch/batchdemo/configuration/BatchConfiguration.java
+++ src/main/java/com/examplebatch/batchdemo/configuration/BatchConfiguration.java
@@ -13,8 +13,11 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.Order;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.PostgresPagingQueryProvider;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
@@ -26,6 +29,8 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 import javax.sql.DataSource;
 import javax.xml.crypto.Data;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableBatchProcessing
@@ -38,15 +43,28 @@ public class BatchConfiguration {
 
     @Bean
     public ItemReader<Person> reader(DataSource dataSource){
-        return new JdbcCursorItemReaderBuilder<Person>()
-                .name("cursorItemReader")
+        return new JdbcPagingItemReaderBuilder<Person>()
+                .name("pagingItemReader")
                 .dataSource(dataSource)
-                .sql(QUERY)
-                .rowMapper((rs, rowNum) -> new Person(
+                .fetchSize(5)
+                .pageSize(5)
+                .rowMapper(((rs, rowNum) -> new Person(
                         rs.getInt(1),
                         rs.getString(2),
-                        rs.getString(3)))
+                        rs.getString(3)
+                )))
+                .queryProvider(queryProvider())
                 .build();
+    }
+
+    private PostgresPagingQueryProvider queryProvider() {
+        final Map<String, Order> sortKeys = new HashMap<>();
+        sortKeys.put("user_id", Order.ASCENDING);
+        final PostgresPagingQueryProvider queryProvider = new PostgresPagingQueryProvider();
+        queryProvider.setSelectClause("user_id, first_name, last_name");
+        queryProvider.setFromClause("users");
+        queryProvider.setSortKeys(sortKeys);
+        return queryProvider;
     }
 
     @Bean
